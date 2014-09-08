7cfb5c05e0c2d729b0f8d4b975c5ebf2e0361a10
Snehal Patil <snehalpatil@yahoo-inc.com>
2014-09-08 10:02:03 -0700

    Submitting Tip Calculator Project.

---
+++ TipCalculatorApp/README.md
@@ -0,0 +1,17 @@
+ToDo App Demo
+
+This is an Android demo application for calculating Tip. It allows user to enter the Bill amount and calculate tip for 10%,15%,20% or user can enter the percentage.
+
+Time spent: 2 hours spent in total
+Completed user stories:
+
+Required:: User is displayed the tip of specified percentage for specified entered amount
+Required:: User enters the total amount of the transaction
+Required:: User can select between tip amounts (i.e 10%, 15%, 20%)
+Required:: Upon selecting tip amount, formatted tip value is displayed
+Optional:: User can select custom tip percentage if desired
+
+
+Walkthrough of all user stories:
+  ![Video Walkthrough](TipCal.gif)
+
---
+++ TipCalculatorApp/TipCalculator/src/com/yahoo/snehalpatil/tipcalculator/TipCalculatorActivity.java
@@ -0,0 +1,69 @@
+package com.yahoo.snehalpatil.tipcalculator;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TipCalculatorActivity extends Activity {
+	public TextView tvTipAmount;
+	public EditText etBillAmt, etTipPert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_calculator);
+    }
+    
+    public void calculate10PertTip(View v) {
+    	calculateTipWithPert(0.10);
+	}
+    
+    public void calculate15PertTip(View v) {
+    	calculateTipWithPert(0.15);
+	}
+    
+    public void calculate20PertTip(View v) {
+    	calculateTipWithPert(0.20);
+	}
+    
+    public void calculateTip(View v) {
+    	etTipPert = (EditText) findViewById(R.id.etTipPert);
+    	String tipPertString = etTipPert.getText().toString();
+    	
+    	// Guard for tipPert
+    	if (tipPertString.matches("")) {
+		    Toast.makeText(this, "You did not enter a Tip Percentage", Toast.LENGTH_SHORT).show();
+		    return;
+		}
+    	
+    	// Calculate tip 
+    	Double tipPert = Double.parseDouble(etTipPert.getText().toString());
+    	calculateTipWithPert(tipPert/100);
+	}
+    
+    private void calculateTipWithPert(Double tipPert){
+    	// Get Bill amount
+    	etBillAmt = (EditText) findViewById(R.id.etBillAmt);
+    	String billAmtStr = etBillAmt.getText().toString();
+    	
+    	// Guard for Bill amount
+		if (billAmtStr.matches("")) {
+		    Toast.makeText(this, "You did not enter a bill amount", Toast.LENGTH_SHORT).show();
+		    return;
+		}
+		
+		// Calculate tip
+    	Double billAmt = Double.parseDouble(billAmtStr);
+    	if (billAmt != 0){
+    		Double totalTip = billAmt  * tipPert;
+        	
+        	// set the total tip amount
+        	tvTipAmount = (TextView) findViewById(R.id.tvTipAmt);
+        	tvTipAmount.setText(String.format( "%.2f", totalTip));
+    	} 
+    }
+}
