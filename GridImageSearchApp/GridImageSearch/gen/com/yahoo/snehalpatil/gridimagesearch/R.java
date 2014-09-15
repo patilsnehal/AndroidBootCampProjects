d29c2b1d9fcc008f1057e9f65a3cf801e2e838aa
Snehal Patil <snehalpatil@yahoo-inc.com>
2014-09-15 00:33:00 -0700

    Added Settings page

--- GridImageSearchApp/GridImageSearch/gen/com/yahoo/snehalpatil/gridimagesearch/R.java
+++ GridImageSearchApp/GridImageSearch/gen/com/yahoo/snehalpatil/gridimagesearch/R.java
@@ -22,32 +22,52 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_settings=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int btnSearch=0x7f080002;
-        public static final int etQuery=0x7f080001;
-        public static final int gvResults=0x7f080003;
-        public static final int ivImage=0x7f080004;
-        public static final int ivImageResult=0x7f080000;
-        public static final int tvTitle=0x7f080005;
+        public static final int action_settings=0x7f080010;
+        public static final int btnSearch=0x7f08000c;
+        public static final int button1=0x7f080009;
+        public static final int etColorFilter=0x7f080002;
+        public static final int etImageSize=0x7f080003;
+        public static final int etImageType=0x7f080006;
+        public static final int etQuery=0x7f08000b;
+        public static final int etSiteFilter=0x7f080008;
+        public static final int gvResults=0x7f08000d;
+        public static final int ivImage=0x7f08000e;
+        public static final int ivImageResult=0x7f08000a;
+        public static final int textView4=0x7f080005;
+        public static final int tvColorFilter=0x7f080004;
+        public static final int tvColorFilter1=0x7f080000;
+        public static final int tvImageSize=0x7f080001;
+        public static final int tvSiteFliter=0x7f080007;
+        public static final int tvTitle=0x7f08000f;
     }
     public static final class layout {
-        public static final int activity_image_display=0x7f030000;
-        public static final int activity_search=0x7f030001;
-        public static final int item_image_result=0x7f030002;
+        public static final int activity_filter=0x7f030000;
+        public static final int activity_image_display=0x7f030001;
+        public static final int activity_search=0x7f030002;
+        public static final int item_image_result=0x7f030003;
     }
     public static final class menu {
-        public static final int image_display=0x7f070000;
+        public static final int filter=0x7f070000;
+        public static final int image_display=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050006;
+        public static final int advancaed_filter_label=0x7f050009;
         public static final int app_name=0x7f050000;
+        public static final int color_filter_label=0x7f05000a;
         public static final int hello_world=0x7f050001;
+        public static final int image_size_label=0x7f050008;
         public static final int image_title_label=0x7f050004;
+        public static final int image_type_label=0x7f05000b;
+        public static final int save_button=0x7f05000d;
         public static final int search_button=0x7f050003;
         public static final int search_promt=0x7f050002;
+        public static final int site_filter_label=0x7f05000c;
+        public static final int title_activity_filter=0x7f050007;
         public static final int title_activity_image_display=0x7f050005;
     }
     public static final class style {
--- GridImageSearchApp/GridImageSearch/src/com/yahoo/snehalpatil/gridimagesearch/SearchActivity.java
+++ GridImageSearchApp/GridImageSearch/src/com/yahoo/snehalpatil/gridimagesearch/SearchActivity.java
@@ -11,6 +11,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -20,6 +22,7 @@ import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.yahoo.snehalpatil.gridimagesearch.adapaters.EndlessScrollListener;
 import com.yahoo.snehalpatil.gridimagesearch.adapaters.ImageResultsAdapter;
 
 public class SearchActivity extends Activity {
@@ -27,12 +30,14 @@ public class SearchActivity extends Activity {
 	private GridView gvResults;
 	private ArrayList<ImageResult> imageResults;
 	private ImageResultsAdapter aImageResults;
-	
+	private Settings settings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         setupViews();
+        setUpSettings();
         
         // Creates the data source
         imageResults = new ArrayList<ImageResult>();
@@ -42,8 +47,27 @@ public class SearchActivity extends Activity {
         
         // Link the adapter to the adapter view i.e grid view
         gvResults.setAdapter(aImageResults);
+        
+        // Attach the listener to the AdapterView onCreate
+        gvResults.setOnScrollListener(new EndlessScrollListener() {
+	    @Override
+	    public void onLoadMore(int page, int totalItemsCount) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to your AdapterView
+	        customLoadMoreDataFromApi(page); 
+                // or customLoadMoreDataFromApi(totalItemsCount); 
+	    }
+        });
     }
     
+ // Append more data into the adapter
+    public void customLoadMoreDataFromApi(int offset) {
+      // This method probably sends out a network request and appends new data items to your adapter. 
+      // Use the offset value and add it as a parameter to your API request to retrieve paginated data.
+      // Deserialize API response and then construct new objects to append to the adapter
+    }
+    
+
     private void setupViews() {
 		// TODO Auto-generated method stub
     	etQuery = (EditText) findViewById(R.id.etQuery);
@@ -64,16 +88,29 @@ public class SearchActivity extends Activity {
 			}
 		});
 	}
+    
+    private void setUpSettings(){
+    	settings = new Settings();
+        settings.imageSize = "small";
+        settings.colorFilter = "red";
+        settings.imageType = "faces";
+        settings.siteFilter = "yahoo.com";
+    }
  
     // Fired whenever the button is pressed 
     public void onImageSearch(View v){
+  	  // URL : https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=android&rez=8
+    	getMoreData();
+    }
+
+	private void getMoreData() {
+
     	String query = etQuery.getText().toString();
-    	Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
-    	  // URL : https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=android&rez=8
-        AsyncHttpClient client = new AsyncHttpClient();
-        String searchURL = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + query + "&rsz=8";
-         
-        client.get(searchURL, new JsonHttpResponseHandler(){
+    	// build URL
+        String url = buildURL(query); 
+        
+        AsyncHttpClient client = new AsyncHttpClient();        
+        client.get(url, new JsonHttpResponseHandler() {
         	@Override
         	public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
         		super.onSuccess(statusCode, headers, response);
@@ -98,6 +135,42 @@ public class SearchActivity extends Activity {
                 System.out.println("statusCode:" + statusCode + "\nerrorResponse:" + errorResponse);
         	};
         });
+    }
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.image_display, menu);
+        return true;
+    }
         
+    public void onSettingsClicked(MenuItem mi){
+    	// construct intent
+    	Intent i = new Intent(this, FilterActivity.class);
+    	// Pass arguments
+    	i.putExtra("settings", settings);
+    	//Execute Intent StartACtivutyForResult
+    	startActivityForResult(i, 5);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	super.onActivityResult(requestCode, resultCode, data);
+    	if(requestCode == 5) {
+    		if(resultCode == RESULT_OK) {
+    			Settings settings = (Settings) data.getSerializableExtra("settings");
+    			this.settings = settings;
+    	    	Toast.makeText(this, settings.siteFilter, Toast.LENGTH_SHORT).show();
+    	    	getMoreData();
+    		}
+    	}
+    }
+    
+    private String buildURL(String query){
+    	// Build URL using settings parameter
+        String searchURL = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8&q=" + query + "&as_sitesearch=" + settings.siteFilter + "&imgcolor="
+        + settings.colorFilter + "&imgsz=" + settings.imageSize + "&imgtype=" + settings.imageType;
+        System.out.println(searchURL);
+        return searchURL;
     }
 }
